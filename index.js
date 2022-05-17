73bd33ccd0233dd565661504e5a01ca912d48da2
imurzaliev <imurzalievi@gmial.com>
2022-05-17 15:50:13 +0300

    Add dropdown function

--- index.js
+++ index.js
@@ -1,31 +1,6 @@
-const container = document.querySelector('.container');
-const navItemTemplate = document.getElementById('nav-item-template');
-const navbarNav = document.querySelector('[data-navbar-nav]');
+const caret = document.querySelector('[data-caret]');
+const dropdownContainer = document.querySelector('[data-dropdown]');
 
-function navItem(item) {
-  const navbarElement = document.importNode(navItemTemplate.content, true);
-  const navItem = navbarElement.querySelector('i');
-  navItem.classList.add(item);
-  navbarNav.appendChild(navbarElement);
-}
-
-function createNavItem(icon) {
-  return {
-    id: generateId(),
-    icon,
-    down: false,
-  };
-}
-
-function generateId() {
-  return Date.now().toString();
-}
-
-const items = ['fa-circle-plus', 'fa-bell', 'fa-comment', 'fa-caret-down'];
-
-(function render() {
-  items.forEach((item) => {
-    const list = createNavItem(item);
-    navItem(list.icon);
-  });
-})();
+caret.addEventListener('click', () => {
+  dropdownContainer.classList.toggle('active');
+});
